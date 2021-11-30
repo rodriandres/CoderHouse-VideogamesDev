b526365ab858b9470b446d1a066cafaddcf9f9ad
rodriandres <rodriandres998@gmail.com>
2021-11-30 20:25:15 -0300

    challenge 12 - ui ingame - complete

---
+++ CoderHouse-Desafio-Implementacion_ingame_de_UI/Assets/Scripts/CoinController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField] private GameManager.typesPoints typePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameManager.typesPoints GetTypesPoints()
+    {
+        return typePoint;
+    }
+}
